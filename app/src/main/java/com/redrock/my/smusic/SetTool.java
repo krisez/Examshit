81a4fd2328c8cf9f782784540f81fd89bbc7574c
krisez <740879437@qq.com>
2016-05-15 18:23:26 +0800

    aaa

--- app/src/main/java/com/redrock/my/smusic/SetTool.java
+++ app/src/main/java/com/redrock/my/smusic/SetTool.java
@@ -1,19 +1,23 @@
 package com.redrock.my.smusic;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 /**
  * Created by ASUS on 2016/5/15.
  */
 public class SetTool extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
 
     }
+
 }
